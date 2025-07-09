1ba9ec2abd75f002d45e287fc10e5c2ed36ed144
Rohit Biseswar <r.k.biseswar@student.tudelft.nl>
2025-07-09 17:02:26 +0200

    Paddings footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <footer class="p-5 bg-[#F9FAFB]">
+    <footer class="p-2 md:p-5 bg-[#F9FAFB]">
         <section class="tracking-tight font-medium text-sm">
             &copy; {{ new Date().getFullYear() }} - Teach Yourself Data Science
         </section>
