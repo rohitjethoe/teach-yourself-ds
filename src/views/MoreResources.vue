7e8dd79b13d3ab69758dace02a72af0ebe824aa8
Rohit Biseswar <r.k.biseswar@student.tudelft.nl>
2025-07-09 16:59:41 +0200

    Letter spacings

--- src/views/MoreResources.vue
+++ src/views/MoreResources.vue
@@ -5,10 +5,10 @@
             More Resources
         </div>
     </section>
-    <section class="p-2 md:p-5">
+    <section class="p-2 md:p-5 tracking-tight">
         I want to sincerely thank all the creators and educators who offer free, high-quality learning materials online. This page—and really this entire project—wouldn’t be possible without their generosity. Below you’ll find a curated list of excellent free resources that I’ve used or recommend for anyone diving into data science and related fields.
     </section>
-    <section class="bg-gray-100 p-2 py-30 md:py-20 md:p-5">
+    <section class="bg-gray-100 p-2 py-30 md:py-20 md:p-5 tracking-tight">
         Hello
     </section>
 </template>
